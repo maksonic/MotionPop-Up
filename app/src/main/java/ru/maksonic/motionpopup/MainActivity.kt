a5f2c1f7f6f2aba437817879f4a52d985cc947f8
maksonic <maksonic.official@gmail.com>
2022-02-16 16:21:14 +0300

    Initial

---
+++ app/src/main/java/ru/maksonic/motionpopup/MainActivity.kt
@@ -0,0 +1,14 @@
+package ru.maksonic.motionpopup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * @Author: maksonic on 16.02.2022
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
